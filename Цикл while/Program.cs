c8b3245fdbba4e9ca43107cecd6ce2e1c0852725
kunic <kunic@DESKTOP-3DUV7TD>
2023-02-15 22:45:19 +0700

    Добавьте файлы проекта.

---
+++ Цикл while/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Здраствуйте! давайте узнаем сколько четный и нечетных чисел в том диапазоне, который вы указали введите первое число");
+int OneNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int TwoNumber = int.Parse(Console.ReadLine());
+while (TwoNumber < 0)
+{
+    TwoNumber++;
+}
